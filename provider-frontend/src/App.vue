d2c56bd55dbf0400f0a7be20858274cf0483302b
ROHL14 <rene.ohl94@gmail.com>
2020-06-05 12:25:49 -0600

    some changes

--- provider-frontend/src/App.vue
+++ provider-frontend/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/provider">Providers</router-link>
+      <router-link to="/">Home</router-link>
+      <span>|</span>
+      <router-link to="/provider">Proveedores</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
